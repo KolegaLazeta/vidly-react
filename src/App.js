e311eb0c272525480f09aee152f56d99ab91f784
Lazar Mladnovic <lazeta998@gmail.com>
2022-11-29 13:17:08 +0100

    Building Login form

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Customers from "./components/cusotmers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navbar";
-import "./App.css";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+import "./App.css";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:_id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
