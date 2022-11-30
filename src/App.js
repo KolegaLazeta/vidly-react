f9a1e411b80b3fa0271778259a5aa2658c254075
Lazar Mladnovic <lazeta998@gmail.com>
2022-11-30 16:33:27 +0100

    Features for adding and searching movies

--- src/App.js
+++ src/App.js
@@ -17,12 +17,12 @@ function App() {
         <Switch>
           <Route path="/register" component={RegisterForm} />
           <Route path="/login" component={LoginForm} />
-          <Route path="/movies/:_id" component={MovieForm} />
+          <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
           <Route path="/not-found" component={NotFound} />
-          <Redirect from="/" to="/movies" exact />
+          <Redirect from="/" exact to="/movies" />
           <Redirect to="/not-found" />
         </Switch>
       </main>
